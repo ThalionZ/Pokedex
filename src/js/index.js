64f6a618cb7205dcb9781eb319025f054825ee74
ThalionZ <99152761+ThalionZ@users.noreply.github.com>
2022-04-03 23:45:01 -0300

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,29 @@
+const listaSelecaoPokemons = document.querySelectorAll(".pokemon");
+const pokemonsCard = document.querySelectorAll(".cartao-pokemon");
+
+listaSelecaoPokemons.forEach((pokemon) => {
+  // Preciso trabalhar com um evento de clique feito pelo usuário na listagem de pokemons
+
+  pokemon.addEventListener("click", () => {
+    //remover a classe aberto só do cartão que está aberto
+    const cartaoPokemonAberto = document.querySelector(".aberto");
+    cartaoPokemonAberto.classList.remove("aberto");
+    //ao clicar em um pokemon da listagem pegamos o id desse pokémon pra saber qual cartão mostrar
+    const idPokemonSelecionado = pokemon.attributes.id.value;
+
+    const idDoCartaoPokemonParaAbrir = "cartao-" + idPokemonSelecionado;
+    const cartaoPokemonParaAbrir = document.getElementById(
+      idDoCartaoPokemonParaAbrir
+    );
+    cartaoPokemonParaAbrir.classList.add("aberto");
+
+    //remover a classe ativo que marca o pokemon selecionado
+    const pokemonAtivoNaListagem = document.querySelector(".ativo");
+    pokemonAtivoNaListagem.classList.remove("ativo");
+
+    //adicionar a classe ativo no item da lista selecionado
+    const pokemonSelecionadoNaListagem =
+      document.getElementById(idPokemonSelecionado);
+    pokemonSelecionadoNaListagem.classList.add("ativo");
+  });
+});
